5168eb3576ad393312b39d81be8a4a546216b78d
Vasif Sultanov <cdm_89@mail.ru>
2024-10-16 10:12:31 +0500

    ddd

--- FirstJavaProject/src/main/java/org/example/App.java
+++ FirstJavaProject/src/main/java/org/example/App.java
@@ -6,14 +6,11 @@ import org.openqa.selenium.remote.DesiredCapabilities;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        System.out.println("Capabilities: " + capabilities);
         System.out.println( "Hello World!" );
     }
-
-    DesiredCapabilities des = new DesiredCapabilities();
     
-
 }
