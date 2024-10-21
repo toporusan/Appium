de23b4cd6d7516c81b4eb7e0b3b62fe0f6ddb63c
Vasif Sultanov <cdm_89@mail.ru>
2024-10-21 17:36:25 +0500

    21.10.2024

---
+++ FirstJavaProject/src/main/java/org/example/AppiumBasics.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AppiumBasics {
+
+    @Test
+    public void appiumTest() {
+
+        URL urlAppium = null;
+        try {
+            urlAppium = new URL("http://127.0.0.1:4723");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName("Android");
+        options.
+
+        AndroidDriver driver = new AndroidDriver(urlAppium, null);
+
+    }
+}
